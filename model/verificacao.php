060e0b7cd63ee46691a15a5f09e7058bb2c90c66
DaviOliveira45 <141350389+DaviOliveira45@users.noreply.github.com>
2024-06-08 09:57:30 -0300

    Atualização (MVC, Lógica VS1.1)

---
+++ model/verificacao.php
@@ -0,0 +1,35 @@
+<?php 
+    //Registrar
+
+    $nome = isset($_POST['nome']) ? $_POST['nome'] : "";
+    $cpf = isset($_POST['cpf']) ? $_POST['cpf'] : "";
+    $email = isset($_POST['email']) ? $_POST['email'] : "";
+    $senha = isset($_POST['senha']) ? $_POST['senha'] : "";
+    $confirmarSenha = isset($_POST['confirmarsenha']) ? $_POST['confirmarsenha'] : "";
+    
+
+    if (isset($_POST['criarconta'])) {
+
+        $nome = $_POST['nome'];
+        $cpf = $_POST['cpf'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        $confirmarSenha = $_POST['confirmarsenha'];
+
+        
+        if (!empty($_POST['nome']) and !empty($_POST['cpf']) and !empty($_POST['email']) and !empty($_POST['senha']) and !empty($_POST['confirmarsenha'])) {
+    
+            if ($_POST['senha'] != $_POST['confirmarsenha']) {
+                echo "<p style='color: red; margin-top: 50px'>As senhas não coincidem!</p>";
+                $senha = "";
+                $confirmarSenha = "";
+            }
+    
+            if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                echo "<p style='color: red'>Email invlálido</p>";
+            }
+        } else {
+            echo "<p style='color: red'>Todos os campos devem estar preenchidos</p>";
+        }
+    }
+?>
--- view/registrar.php
+++ view/registrar.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrar | GamerShop</title>
 
-    <link rel="stylesheet" href="../css/style.css"/>
-    <link rel="stylesheet" href="../css/styleregistro.css"/>
-    <script src="../java/interacao.js"></script>
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="css/styleregistro.css"/>
 
 </head>
 <body>
@@ -27,7 +26,7 @@
                 <input type="text" placeholder="Busque seu jogo..." id="search"/>
             </section>
             <aside id="pesquisa1">
-                <img id="pesquisadesktop" src="../imagens/imgpesquisar_resized.png"/>
+                <img id="pesquisadesktop" src="imagens/imgpesquisar_resized.png"/>
             </aside>    
         </div>
         <ul id="entrar-registro">
@@ -36,15 +35,10 @@
 
     </nav>
 
-    <img id="logo" src="../imagens/G mer (200 x 112 px).png"/>
-    <img id="logomobile" src="../imagens/logo_gamershop (63 x 63 px).png"/>
-
-    <div id="submenubotao">
-        <img src="../imagens/imgbotao-att.png"/>
-    </div>
+    <img id="logo" src="imagens/G mer (200 x 112 px).png"/>
 
     <div id="sacoladesktop">
-        <img src="../imagens/sacola(60 x 40).png"/>
+        <img src="imagens/sacola(60 x 40).png"/>
     </div>
 
     </header>
@@ -53,20 +47,24 @@
 
         <h2>Crie sua conta</h2>
         <h5>e concorra a promoções incríveis!</h5>
+
+        <?php 
+            require_once("../model/verificacao.php");
+        ?>
         
-        <form action="" method="post">
+        <form action="" method="POST">
             
-            <input type="text" name="nome" placeholder="Nome completo" id="nome"/>
+            <input type="text" name="nome" value="<?php echo $nome; ?>" placeholder="Nome completo" id="nome"/>
 
-            <input type="text" name="cpf" maxlength="11" placeholder="CPF" id="cpf"/>
+            <input type="text" name="cpf" value="<?php echo $cpf; ?>" maxlength="11" placeholder="CPF" id="cpf"/>
 
-            <input type="text" name="email" placeholder="E-mail" id="email"/>
+            <input type="text" name="email" value="<?php echo $email; ?>" placeholder="E-mail" id="email"/>
 
-            <input type="text" name="senha" placeholder="Senha" id="senha"/>
+            <input type="text" name="senha" value="<?php echo $senha; ?>" placeholder="Senha" id="senha"/>
 
-            <input type="text" name="confirmarsenha" placeholder="Confirmar senha" id="confirmasenha"/>
+            <input type="text" name="confirmarsenha" value="<?php echo $confirmarSenha; ?>" placeholder="Confirmar senha" id="confirmasenha"/>
 
-            <div id="check-box">
+            <!-- <div id="check-box">
                 <section id="corpocheck">
                     <input type="checkbox" id="termosuso"/>
                     <label for="termosuso" class="checkpersonalizado"></label>
@@ -74,9 +72,9 @@
                 <aside id="textolateral">
                     <p>Eu li e concordo com os <a href="termosdeuso.html">Termos de Uso</a> e <a href="politicadeprivacidade.html">Política de Privacidade</a>. A GamerShop não coleta intencionalmente dados de crianças menores de 13 anos de idade.</p>
                 </aside>
-            </div>
+            </div> -->
 
-            <p><button type="submit" name="criarconta" id="criarbutton">CRIAR CONTA</button></p>
+            <p><button type="submit" name="criarconta" value="criarconta" id="criarbutton">CRIAR CONTA</button></p>
         </form>
 
         <p id="comconta">Já tem conta?</p>
