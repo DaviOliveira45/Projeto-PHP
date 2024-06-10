65df5e230d9d43def617618c018aa73eb16bc99d
DaviOliveira45 <141350389+DaviOliveira45@users.noreply.github.com>
2024-06-10 11:24:37 -0300

    Atualização (Adição de CSS nas tabelas)

--- view/excluir.php
+++ view/excluir.php
@@ -9,7 +9,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Excluir</title>
-    <link rel="stylesheet" href="View/style.css">
+    <link rel="stylesheet" href="view/css/style.css">
+    <link rel="stylesheet" href="view/css/styletable.css">
 </head>
 <body>
     <h1>Excluir Clientes</h1>
--- view/listar.php
+++ view/listar.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listar</title>
-    <link rel="stylesheet" href="View/style.css">
+    <link rel="stylesheet" href="view/css/style.css">
+    <link rel="stylesheet" href="view/css/styletable.css">
 </head>
 <body>
     <h1>Listar Clientes</h1>
